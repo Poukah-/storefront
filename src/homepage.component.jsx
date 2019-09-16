18789c67f1869868d73ec78ec3b6e0403a4e8b41
Poukah <petersson.john@gmail.com>
2019-09-17 01:06:04 +0200

    Created project with create-react-app and added files for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+
+      <div className='menu-item'>
+			  <div className='content'>
+          <div className='content'>
+            <h1 className='title'>SKATEBOARDS</h1>
+            <span className='subtitle'>SHOW NOW</span>
+          </div>
+        </div>
+      </div>
+
+      <div className='menu-item'>
+			  <div className='content'>
+          <div className='content'>
+            <h1 className='title'>SNEAKERS</h1>
+            <span className='subtitle'>SHOW NOW</span>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='menu-item'>
+			  <div className='content'>
+          <div className='content'>
+            <h1 className='title'>JACKETS</h1>
+            <span className='subtitle'>SHOW NOW</span>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='menu-item'>
+			  <div className='content'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOW NOW</span>
+          </div>
+        </div>
+      </div>
+
+
+      <div className='menu-item'>
+			  <div className='content'>
+          <div className='content'>
+            <h1 className='title'>ROUGHWEAR</h1>
+            <span className='subtitle'>SHOW NOW</span>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+);
+
+export default HomePage;
