f7f1266aaa3ee44f214ca515e0a96a91f6482d94
Poukah <petersson.john@gmail.com>
2019-09-18 23:53:50 +0200

    Forms & components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 const HatsPage = () => (
@@ -20,6 +21,7 @@ function App() {
       <Switch> // if wrapped in Switch: as soon as it finds a route that fits it won't proceed with the others
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
