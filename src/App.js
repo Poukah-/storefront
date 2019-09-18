54f553d16f08934442164689840101eff52d0bde
Poukah <petersson.john@gmail.com>
2019-09-18 22:05:33 +0200

    updating our app with a shop page && collection-preview.component && collection-item.component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -16,7 +17,7 @@ function App() {
     <div>
       <Switch> // if wrapped in Switch: as soon as it finds a route that fits it won't proceed with the others
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
